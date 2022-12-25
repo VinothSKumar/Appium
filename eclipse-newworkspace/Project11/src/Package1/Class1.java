ba20dd5249fc8e8c4f501f73896c9ea24ee6bf53
VinothSKumar <121083239+VinothSKumar@users.noreply.github.com>
2022-12-25 22:33:55 +0530

    main method

--- eclipse-newworkspace/Project11/src/Package1/Class1.java
+++ eclipse-newworkspace/Project11/src/Package1/Class1.java
@@ -1,9 +1,10 @@
 package Package1;
 
 public class Class1 {
-private void method() {
-	// TODO Auto-generated method stub
-System.out.println("pushing code to git");
-System.out.println("Second line of Code");
-}
+	public static void main(String[] args) {
+		System.out.println("pushing code to git");
+		System.out.println("Second line of Code");
+	}
+		
+	
 }
